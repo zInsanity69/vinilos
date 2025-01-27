f01845dd2ed53d88121e1f28cdd8a2a6cd58afdd
Fidel <fidellauheinz@gmail.com>
2025-01-27 19:36:48 +0100

    archivo cargarDiscos.js recuperado

---
+++ cargarDiscos.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $.ajax({
+        url:"cargarDiscos.php",
+        type:"GET",
+        success:function(response){
+            console.log(response)
+            $('#vinylContainer').html(response);
+        },
+        error:function(){
+            $('#mensaje').html('Error al cargar los elementos.');
+        }
+    })
+})
