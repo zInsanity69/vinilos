867c3bf0302c3b72e647d6fbead9ad174dc16665
locura <rubenmotos2002@gmail.com>
2025-01-30 12:19:30 +0100

    cambio de datos de acceso de la base de datos

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$host = "localhost";
-$user = "root";
-$pass = ""; // Tu contraseña de MySQL
-$db   = "tiendavinilos";
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD'); // Tu contraseña de MySQL
+$db   = getenv('MYSQLDATABASE');
 
 $con = new mysqli($host, $user, $pass, $db);
 
